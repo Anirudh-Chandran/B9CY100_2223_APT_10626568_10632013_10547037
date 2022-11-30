66543b97d8e59ae9ced0446e050f12beec132f9e
Anirudh <116883902+Anirudh-Chandran@users.noreply.github.com>
2022-11-30 11:43:55 +0000

    Update Middleware_Code.py

--- Python_Code/Middleware_Code.py
+++ Python_Code/Middleware_Code.py
@@ -13,8 +13,6 @@ username ='SAadmin'
 password ='Dublin@098'
 connection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';ENCRYPT=yes;UID='+username+';PWD='+ password)
 cursor = connection.cursor()
-cursor.execute("INSERT INTO Vendor (v_id,v_name) VALUES('4','rajeev');")
-connection.commit()
 
 cursor.execute("SELECT * FROM Vendor;")
 for row in cursor.fetchall():
