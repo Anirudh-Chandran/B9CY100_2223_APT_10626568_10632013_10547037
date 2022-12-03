2774e2a648a2fb6001e0d90b837163826c62fa3e
Anirudh <116883902+Anirudh-Chandran@users.noreply.github.com>
2022-12-03 16:12:11 +0000

    Update Middleware_Code.py

--- Python_Code/Middleware_Code.py
+++ Python_Code/Middleware_Code.py
@@ -35,8 +35,10 @@ def Database_Connection(binary_value):
     password ='Dublin@098'
     connection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';ENCRYPT=yes;UID='+username+';PWD='+ password)
     cursor = connection.cursor()
-    command = "INSERT INTO Images(col_image) VALUES(?)"
+    command = "INSERT INTO Images(col_image) VALUES('?')"
     cursor.execute(command,binary_value)
+    cursor.commit()
+    command = "SELECT * from Images;"
     cursor.commit()
     for row in cursor.fetchall():
         print(row)
