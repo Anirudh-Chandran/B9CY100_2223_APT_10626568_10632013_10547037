b074d5e0ef6ba963a0501648d17991d79bb9069a
Anirudh <116883902+Anirudh-Chandran@users.noreply.github.com>
2022-12-03 14:58:09 +0000

    Update Middleware_Code.py

--- Python_Code/Middleware_Code.py
+++ Python_Code/Middleware_Code.py
@@ -1,23 +1,45 @@
-from flask import Flask
+from flask import Flask,request,render_template
+import os
 import pyodbc
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
+app.config['UPLOADED_IMAGES_DEST'] = 'uploads'
+
+
 @app.route("/")#URL leading to method
-def hello(): # Name of the method
-    return("Hello World!") #indent this line
+def index():  # Name of the method
+    return render_template("index.html")
 
-server = 'tcp:avadb01.database.windows.net'
-database ='AVA_DB_1'
-username ='SAadmin'
-password ='Dublin@098'
-connection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';ENCRYPT=yes;UID='+username+';PWD='+ password)
-cursor = connection.cursor()
 
-cursor.execute("SELECT * FROM Vendor;")
-for row in cursor.fetchall():
-    print(row)
+@app.route("/submission", methods=['GET', 'POST'])
+def submission():
+    if request.method == "POST":
+        fname = request.form['fname']
+        files = request.files['photo']
+        file_name = files.filename
+        files.save(os.path.join(app.config['UPLOADED_IMAGES_DEST'],file_name))
+        with open(os.path.join(app.config['UPLOADED_IMAGES_DEST'],file_name), 'rb') as f:
+            bin_data = f.read()
+        Database_Connection(bin_data)
+        return render_template('submission.html')
+    else:
+        return "except"
 
+
+def Database_Connection(binary_value):
+    server = 'tcp:avadb01.database.windows.net'
+    database ='AVA_DB_1'
+    username ='SAadmin'
+    password ='Dublin@098'
+    connection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';ENCRYPT=yes;UID='+username+';PWD='+ password)
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO Product(Prod_id, Prod_Name, Prod_Man_Date, Prod_Man_By, Prod_Dimensions, Prod_Image) VALUES('1', 'X-RAY', 2021-02-28, 'Pharma', '10x10x10','" + binary_value +"');")
+    cursor.commit()
+    for row in cursor.fetchall():
+        print(row)
+
+        
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080) # indent this line
 
