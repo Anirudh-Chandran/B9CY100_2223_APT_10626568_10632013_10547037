c15a8c8df752f0920092f6ffc253e87de3416b04
Anirudh <116883902+Anirudh-Chandran@users.noreply.github.com>
2022-12-04 19:18:41 +0000

    Update Middleware_Code.py

--- Python_Code/Middleware_Code.py
+++ Python_Code/Middleware_Code.py
@@ -23,7 +23,10 @@ def submission():
             bin_data = f.read()
         Database_Connection(bin_data)
         image_val = Data_Retrieval(bin_data)
-        with open('image.jpg',"wb") as f:
+
+        image = 'image.jpg'
+        image_new_loc = image_loc + image
+        with open(image_new_loc,"wb") as f:
             f.write(image_val)
         return render_template('submission.html', image=image_new_loc)
     else:
