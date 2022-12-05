a5051069519b176cc8d3e47f31a7dcc56a1f7728
Vicky Motwani <10632013@mydbs.ie>
2022-12-05 14:08:38 +0000

    Create Request_approval.sql

---
+++ SQL_Queries/Request_approval.sql
@@ -0,0 +1,8 @@
+create table Request_Approval(
+Ack_ID int not null, 
+V_ID int not null,
+Req_ID int not null,
+Ack_status varchar not null,
+foreign key (V_ID) references vendor,
+foreign key (V_ID) references vendor,
+)
