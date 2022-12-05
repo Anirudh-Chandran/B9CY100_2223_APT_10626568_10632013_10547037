6e497678a284d5dbd5290e7a86aa103ade67635f
Vicky Motwani <10632013@mydbs.ie>
2022-12-05 14:47:26 +0000

    Update Request_approval.sql

--- SQL_Queries/Request_approval.sql
+++ SQL_Queries/Request_approval.sql
@@ -1,4 +1,5 @@
-create table Request_Approval(
+
+/*create table Request_Approval(
 Ack_ID int not null, 
 V_ID int not null,
 Req_ID int not null,
@@ -7,3 +8,7 @@ foreign key (V_ID) references vendor,
 --foreign key (Req_ID) references OnDemand_Request,
 primary key (Ack_ID)
 )
+*/
+
+alter table request_approval
+drop constraint FK__Request_Ap__V_ID__31B762FC
