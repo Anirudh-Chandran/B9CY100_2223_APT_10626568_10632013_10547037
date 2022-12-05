8f7678895d7b9917229e79ac75774e10514bd103
Vicky Motwani <10632013@mydbs.ie>
2022-12-05 13:11:22 +0000

    Update Prod_Images.sql

--- SQL_Queries/Prod_Images.sql
+++ SQL_Queries/Prod_Images.sql
@@ -7,9 +7,9 @@ Prod_Image varbinary
 
 --select * from Prod_Images
 
---Alter table prod_images
---alter column Img_ID int not null
-
-
 Alter table prod_images
-add primary key(Img_id)
+add Prod_ID int foreign key (Prod_ID) references Product
+
+
+--Alter table prod_images
+--add primary key(Img_id)
