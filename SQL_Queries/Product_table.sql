2599f412467ba851035f62a53434d1bc93a4a30b
Vicky Motwani <10632013@mydbs.ie>
2022-12-05 18:20:57 +0000

    Update Product_table.sql

--- SQL_Queries/Product_table.sql
+++ SQL_Queries/Product_table.sql
@@ -25,9 +25,10 @@ alter table Product
 --alter column prod_id int not null
 --add primary key(prod_id)
 --drop constraint FK__Product__v_id__3A4CA8FD
-add constraint v_id_fk foreign key (v_id) references vendor
+--add constraint v_id_fk foreign key (v_id) references vendor
+alter column prod_description varchar(1000) not null
 
---select * from product
+select * from product
 --delete from Product
 
 
