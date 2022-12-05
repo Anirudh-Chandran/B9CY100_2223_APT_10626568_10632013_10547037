ccfaba3776d2d42a4b61c6ded7cd5f01cfd3125b
Vicky Motwani <10632013@mydbs.ie>
2022-12-05 12:52:10 +0000

    Update On-Demand_request.sql

--- SQL_Queries/On-Demand_request.sql
+++ SQL_Queries/On-Demand_request.sql
@@ -1,11 +1,13 @@
 create table OnDemand_Request(
-Req_ID int,
-Req_Title varchar,
+Req_ID int(3) not null,
+Req_Title varchar not null,
 Req_Quantity int(2),
 Req_Dimensions varchar,
-Req_Description varchar,
+Req_Description varchar not null,
 Req_Need_by_Date date,
-Req_Budget varchar,
-H_ID int,
-foreign key(H_ID)
-references hospital)
+Req_Budget varchar not null,
+primary key (Req_ID)
+--H_ID int,
+--foreign key(H_ID)
+--references hospital
+)
