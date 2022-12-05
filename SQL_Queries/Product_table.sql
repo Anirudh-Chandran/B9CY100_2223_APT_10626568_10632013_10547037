0fe26b64a31108141b2272c73c83619810f1dde6
Vicky Motwani <10632013@mydbs.ie>
2022-12-05 12:41:07 +0000

    Update Product_table.sql

--- SQL_Queries/Product_table.sql
+++ SQL_Queries/Product_table.sql
@@ -1,7 +1,8 @@
+/*
 Create table Product (
 Prod_id varchar(10) not null,
 Prod_Name varchar(200) not null,
-Prod_Man_Date datetime not null,
+Prod_Man_Date date not null,
 Prod_Man_By varchar(200) not null,
 Prod_Dimensions varchar(200) not null, 
 Prod_Quantity int not null,
@@ -12,3 +13,11 @@ CONSTRAINT Prod_pk PRIMARY KEY (Prod_id),
 CONSTRAINT v_id_fk foreign key (v_id)
 REFERENCES Vendor (v_id)
 )
+*/
+--insert into Product (prod_id, prod_name, prod_quantity,v_id)
+--values('123','Nam', 7,4)
+
+
+
+alter table product
+Add prod_description varchar
