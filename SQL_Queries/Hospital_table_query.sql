97ac6cfaf25144034fff5af8e8b185ea6e1a0e7a
Vicky Motwani <10632013@mydbs.ie>
2022-12-05 13:49:22 +0000

    Update Hospital_table_query.sql

--- SQL_Queries/Hospital_table_query.sql
+++ SQL_Queries/Hospital_table_query.sql
@@ -12,5 +12,5 @@ alter table hospital
 --alter column H_Id int not null
 --alter column h_name varchar not null
 --drop column h_contact
-alter column Phome varchar
+rename column Email to H_Email
 --add primary key(H_ID)
