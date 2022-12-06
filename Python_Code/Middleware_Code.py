7ac2df473a488f12dd912bb60dc2dca76e00679e
Anirudh <116883902+Anirudh-Chandran@users.noreply.github.com>
2022-12-06 19:21:13 +0000

    login and registeration

--- Python_Code/Middleware_Code.py
+++ Python_Code/Middleware_Code.py
@@ -1,17 +1,41 @@
 import flask
-from flask import Flask,request,render_template,url_for
+from flask import Flask,request,render_template,url_for,flash
 import os
 import pyodbc
 
 app = Flask(__name__, template_folder="templates")
 app.config['UPLOADED_IMAGES_DEST'] = 'static'
+app.config['SECRET_KEY'] = "secret_key"
 
 
 @app.route("/")
-@app.route("/Home")
-def Homepage():
+@app.route("/Home", methods=['GET', 'POST'])
+def homepage():
     return render_template("Homepage.html")
 
+
+@app.route("/Login", methods=['GET', 'POST'])
+def loginpage():
+    login_ids = {'admin':'password'}
+    if request.method == "POST":
+        username = request.form['uname']
+        password = request.form['pwd']
+        if username in login_ids:
+            if password == login_ids[username]:
+                return render_template("Homepage.html")
+            else:
+                message = 'PASSWORD is incorrect'
+                flash(message)
+                return render_template("Login.html")
+        else:
+            flash('NO Username found, Please register...')
+            return render_template("Login.html")
+    else:
+        return render_template("Login.html")
+
+@app.route("/Register")
+def register():
+    return render_template("Register.html")
 
 # @app.route("/submission", methods=['GET', 'POST'])
 # def submission():
