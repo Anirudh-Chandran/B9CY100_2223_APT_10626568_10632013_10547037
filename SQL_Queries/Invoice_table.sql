6d2e58b98aae6006f9f2ea75e7e0bf4cac654ebd
Vicky Motwani <10632013@mydbs.ie>
2022-12-10 14:23:44 +0000

    Update Invoice_table.sql

--- SQL_Queries/Invoice_table.sql
+++ SQL_Queries/Invoice_table.sql
@@ -13,11 +13,13 @@ H_ID		INT			NOT NULL,
 alter table invoice
 --add primary key (inv_id)
 --add foreign key (V_ID) references vendor
---add foreign key (h_ID) references hospital
-drop constraint FK__Invoice__Prod_ID__37703C52
+add foreign key (h_ID) references hospital
+--drop constraint FK__Invoice__Prod_ID__37703C52
 --drop column v_id
 --add foreign key (Prod_id) references product
-alter column prod_name varchar (200)
+--alter column prod_name varchar (200)
+
+add Inv_id int identity(1,1) primary key
 
 
 SELECT TABLE_NAME,
